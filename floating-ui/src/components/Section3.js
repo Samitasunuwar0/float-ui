de2771c63da9858013e68440f03dac1d35d94c67
Samitasunuwar0 <108125270+Samitasunuwar0@users.noreply.github.com>
2023-02-05 19:19:00 +0545

    main

---
+++ floating-ui/src/components/Section3.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Section3.css';
+const Section3 = () => {
+  return (
+    <div className='section-3'>
+      <form className='form-3'>
+      <div className='content-3'>
+        <img src='https://www.motortrend.com/uploads/2022/03/2022-Honda-Civic-Touring-vs-2022-Hyundai-Elantra-Limited-vs-2022-Kia-Forte-GT-vs-2022-Mazda-Mazda3-Sedan-AWD-Turbo-vs-2022-Nissan-Sentra-SR-vs-2022-Volkswagen-Jetta-SEL-19.jpg?fit=around%7C875:492' alt='car'/>
+          <p> <h2>Cars</h2>A car or automobile is a motor vehicle with wheels. Most definitions of cars say that they run primarily on roads, seat one to eight people, have four wheels, and mainly transport people (rather than goods).Cars were invented in 1886, 
+            when German inventor Carl Benz patented his Benz Patent-Motorwagen. Cars became widely available during the 20th century. One of the first cars affordable by the masses was the 1908 Model T, an American car manufactured by the Ford Motor 
+            Company. Cars were rapidly adopted in the US, where they replaced horse-drawn carriages. In Europe and other parts of the world, demand for automobiles did not increase until after World War II.[7] The car is considered an essential part of the 
+            developed economy.</p>
+          </div>
+      </form>
+    </div>
+    
+  )
+}
+
+export default Section3
